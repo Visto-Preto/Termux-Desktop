322fcac939936ec27cac08288cf4fb847e108c15
Visto Preto <evppdepf@protonmail.ch>
2023-01-20 13:08:15 -0300

    update

--- setup.sh
+++ setup.sh
@@ -7,9 +7,11 @@ cd $HOME
 echo -e '\e[1;36m Atualizando e baixando pacotes e suas dependencias \e[1m'
 echo -e '''
 \e[1;31m A velocidade de download depende da 
- sua conexao com a internet \e[1m'''
+ sua conexao com a internet \e[1m
+ 
+ '''
 
-sleep 2
+sleep 1
 clear
 
 apt update && apt upgrade -y
@@ -20,5 +22,5 @@ clear
 cowsay -f eyes Termux Desktop | lolcat
 echo -e ' '
 curl -sLf https://raw.githubusercontent.com/Visto-Preto/Termux-Desktop/master/about/about | lolcat
-echo -e 'Version : '
-curl -sLf https://raw.githubusercontent.com/Visto-Preto/Termux-Desktop/master/about/version | lolcat
+echo -e 'Version : '$(curl -sLf https://raw.githubusercontent.com/Visto-Preto/Termux-Desktop/master/about/version) | lolcat
+echo ' '
