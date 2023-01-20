57855e40c6abc959212aeedc03bbd3879830fc4c
Leonardo Sousa <45713275+Visto-Preto@users.noreply.github.com>
2023-01-20 10:59:56 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,19 @@
 ## Mail    : evppdepf@protonmail.ch
 ## Github  : @Visto-Preto
 
+clear
 
-echo -e "Teste ok"
+cd $HOME
+
+echo -e '\e[1;36m installing packages, it is necessary  \e[1m'
+echo -e '''
+\e[1;31m Download speed depends on
+ your internet connection \e[1m'''
+
+sleep 3
+
+clear
+
+echo -e '\e[1;36m installing xfce4 and basic apps...\e[1m'
+
+sleep 1
